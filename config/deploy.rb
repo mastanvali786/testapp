d111f09bbbcde276f1701ea63647adb414ecbbc2
Shaik Mastanvali <shaikmastanvali787@gmail.com>
2016-05-18 19:49:20 +0530

    added files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 # config valid only for current version of Capistrano
 lock '3.5.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
-set :deploy_to, '/var/www/my_app_name'
+set :application, 'testapp'
+set :repo_url, 'git@github.com:mastanvali786/testapp.git'
+set :deploy_to, '/opt/www/testapp'
 set :user, 'deploy'
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets}
 
@@ -39,6 +39,17 @@ set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets}
 # set :keep_releases, 5
 
 namespace :deploy do
+
+	%w[start stop restart].each do |command|
+		desc 'Manage Unicorn'
+		task command do
+			on roles(:app), in: :sequence, wait: 1 do
+				execute "/etc/init.d/unicorn_#{fetch(:application)} #{command}"
+			end
+		end
+	end
+
+	after :publishing, :restart
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
